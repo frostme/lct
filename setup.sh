e54902998362aa7247b017284a12dea72f67cafa
M. Elliot Frost <melliotfrost@gmail.com>
2026-02-07 16:46:33 -0500

    new setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
+if command -v mise &>/dev/null; then
+  echo "mise is already installed"
+else
+  cargo install cargo-binstall
+  cargo binstall mise
+fi
+
 brew bundle
-gem install bashly
+mise install
