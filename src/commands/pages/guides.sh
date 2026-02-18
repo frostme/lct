e135871591f3f02a1cb6b976edff14a711753721
M. Elliot Frost <melliotfrost@gmail.com>
2026-02-18 17:09:06 -0500

    pages overhaul start

---
+++ src/commands/pages/guides.sh
@@ -0,0 +1,5 @@
+echo "# This file is located at 'src/commands/pages/guides.sh'."
+echo "# It contains the implementation for the 'lct pages guides' command."
+echo "# The code you write here will be wrapped by a function named 'lct_pages_guides_command()'."
+echo "# Feel free to edit this file; your changes will persist when regenerating."
+inspect_args
---
+++ src/commands/pages/reference/configuraion.sh
@@ -0,0 +1,5 @@
+echo "# This file is located at 'src/commands/pages/reference/configuraion.sh'."
+echo "# It contains the implementation for the 'lct pages reference configuraion' command."
+echo "# The code you write here will be wrapped by a function named 'lct_pages_reference_configuraion_command()'."
+echo "# Feel free to edit this file; your changes will persist when regenerating."
+inspect_args
